efc4e42c10e981ee3f30d921749b8ea4d064dd1e
Bijoy Anil <86096110+bijoy7034@users.noreply.github.com>
2023-02-11 10:49:41 +0530

    eg

--- js/cards.js
+++ js/cards.js
@@ -1,3 +1,13 @@
 function nav_contact(){
-    windows.open('contact.html', '_self')
+    var tempParams = {
+        from_name: document.getElementById('email1').value,
+        email_id: document.getElementById('email1').value,
+        message:document.getElementById('message1').value,
+        mobile_no:document.getElementById("mobile_no").value,
+    };
+
+    emailjs.send("service_76531l9","template_uno22oj",tempParams)
+    .then(function(res){
+        document.getElementById('contact_if').innerHTML = "You will get a phone call soon.."
+    })
 }
